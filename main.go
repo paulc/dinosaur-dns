ed89131af97cb82d96203139b603ce34d45b0f3e
paulc <paulc@>
2022-07-20 14:10:32 +0100

    Allow multiple listenAddr

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 type ProxyConfig struct {
-	ListenAddr    string
+	ListenAddr    []string
 	Upstream      string
 	DOHUpstream   bool
 	FilterAll     bool
@@ -68,19 +68,17 @@ func dohRequestProxy(w dns.ResponseWriter, r *dns.Msg, upstream string) {
 		return
 	}
 
-	log.Printf("Request: %s (DoH)", r.Question[0].Name)
 	// Directly write message (we dont need to decode locally)
 	w.Write(buffer.Bytes())
 }
 
 func dnsRequestProxy(w dns.ResponseWriter, r *dns.Msg, upstream string) {
 	c := new(dns.Client)
-	out, rtt, err := c.Exchange(r, upstream)
+	out, _, err := c.Exchange(r, upstream)
 	if err != nil {
 		dnsError(w, r, dns.RcodeServerFailure, err)
 		return
 	}
-	log.Printf("Request: %s (%s)", r.Question[0].Name, rtt)
 	w.WriteMsg(out)
 }
 
@@ -106,9 +104,11 @@ func makeHandler(config ProxyConfig) func(dns.ResponseWriter, *dns.Msg) {
 
 		name := r.Question[0].Name
 		if (r.Question[0].Qtype == dns.TypeAAAA) && (config.FilterAll || matchDomain(config.FilterDomains, name)) {
-			dnsError(w, r, dns.RcodeNameError, fmt.Errorf("Request: %s (aaaa blocked)", name))
+			dnsError(w, r, dns.RcodeNameError, fmt.Errorf("%s %s (filtered)", name, dns.Type(r.Question[0].Qtype).String()))
 			return
 		}
+
+		log.Printf("%s %s", name, dns.Type(r.Question[0].Qtype).String())
 
 		if config.DOHUpstream {
 			dohRequestProxy(w, r, config.Upstream)
@@ -122,7 +122,7 @@ func main() {
 
 	// Command line flags
 
-	var listenFlag = flag.String("listen", "127.0.0.1:8053", "Listen address (default: 127.0.0.1:8053)")
+	var listenFlag = flag.String("listen", "127.0.0.1:8053", "Listen address (comma separated) (default: 127.0.0.1:8053)")
 	var upstreamFlag = flag.String("upstream", "1.1.1.1:53", "Upstream resolver (default: 1.1.1.1:53)")
 	var filterAllFlag = flag.Bool("filter-all", false, "Filter all AAAA requests (default: false)")
 	var filterDomainFlag = flag.String("filter-domains", "", "Filter AAAA requests for matching domains (comma-separated) (default: \"\")")
@@ -139,11 +139,16 @@ func main() {
 	// Initialise config
 
 	config := ProxyConfig{
-		ListenAddr:    *listenFlag,
+		ListenAddr:    make([]string, 0),
 		Upstream:      *upstreamFlag,
 		DOHUpstream:   strings.HasPrefix(*upstreamFlag, "https://"),
 		FilterAll:     *filterAllFlag,
 		FilterDomains: make([]string, 0),
+	}
+
+	// Get listen address
+	for _, v := range strings.Split(*listenFlag, ",") {
+		config.ListenAddr = append(config.ListenAddr, v)
 	}
 
 	// Get filter domains from command line (comma separated)
@@ -171,34 +176,36 @@ func main() {
 		}
 	}
 
-	// Start UDP server
-	server_udp := &dns.Server{
-		Addr: config.ListenAddr,
-		Net:  "udp",
-	}
-
-	go func() {
-		if err := server_udp.ListenAndServe(); err != nil {
-			log.Fatal(err)
+	for _, listenAddr := range config.ListenAddr {
+		// Start UDP server
+		server_udp := &dns.Server{
+			Addr: listenAddr,
+			Net:  "udp",
 		}
-	}()
 
-	// Start TCP server
-	server_tcp := &dns.Server{
-		Addr: config.ListenAddr,
-		Net:  "tcp",
-	}
+		go func() {
+			if err := server_udp.ListenAndServe(); err != nil {
+				log.Fatal(err)
+			}
+		}()
 
-	go func() {
-		if err := server_tcp.ListenAndServe(); err != nil {
-			log.Fatal(err)
+		// Start TCP server
+		server_tcp := &dns.Server{
+			Addr: listenAddr,
+			Net:  "tcp",
 		}
-	}()
+
+		go func() {
+			if err := server_tcp.ListenAndServe(); err != nil {
+				log.Fatal(err)
+			}
+		}()
+	}
 
 	// Handle requests
 	dns.HandleFunc(".", makeHandler(config))
 
-	log.Printf("Started server: %s", config.ListenAddr)
+	log.Printf("Started server: %s", strings.Join(config.ListenAddr, " "))
 	log.Printf("Upstream: %s (DOH=%t)", config.Upstream, config.DOHUpstream)
 	log.Printf("Filter All: %t", config.FilterAll)
 	log.Printf("Filter Domains: %s", strings.Join(config.FilterDomains, " "))
